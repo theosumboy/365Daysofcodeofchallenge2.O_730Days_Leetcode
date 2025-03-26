3d759be3aa5a0f7e2d356828767e153b1400b93b
Akshat Sinha <112637568+theosumboy@users.noreply.github.com>
2025-03-26 21:26:52 +0530

    Minimum Operations to Make a Uni-Value Grid
    You are given a 2D integer grid of size m x n and an integer x. In one operation, you can add x to or subtract x from any element in the grid.
    
    A uni-value grid is a grid where all the elements of it are equal.
    
    Return the minimum number of operations to make the grid uni-value. If it is not possible, return -1.
    
     
    
    Example 1:
    
    
    Input: grid = [[2,4],[6,8]], x = 2
    Output: 4
    Explanation: We can make every element equal to 4 by doing the following: 
    - Add x to 2 once.
    - Subtract x from 6 once.
    - Subtract x from 8 twice.
    A total of 4 operations were used.
    Example 2:
    
    
    Input: grid = [[1,5],[2,3]], x = 1
    Output: 5
    Explanation: We can make every element equal to 3.
    Example 3:
    
    
    Input: grid = [[1,2],[3,4]], x = 2
    Output: -1
    Explanation: It is impossible to make every element equal.
     
    
    Constraints:
    
    m == grid.length
    n == grid[i].length
    1 <= m, n <= 105
    1 <= m * n <= 105
    1 <= x, grid[i][j] <= 104

---
+++ Day556.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minOperations(self, grid, x):
+        a = [cell for row in grid for cell in row]
+        a.sort()
+        mid = len(a) // 2
+        median = a[mid]
+        tc = 0
+        for value in a:
+            if abs(value - median) % x != 0:
+                return -1
+            tc += abs(value - median) // x
+        return tc
+        
